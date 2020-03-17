6fdec915778e43d16d226cc8d3a8cf9448df0636
Nathanael Fletcher <nathanaelfletcher19@gmail.com>
2020-03-17 17:20:48 -0400

    added edit and delete api calls. Untested, draft one

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import UpdateCaregiver from './components/UpdateCaregiver.js'
 
 let baseURL = process.env.REACT_APP_BASEURL
 
+// Determines baseURL for dev or deployment
 if (process.env.NODE_ENV === 'development') {
   baseURL = 'http://localhost:3003'
 } else {
@@ -23,15 +24,19 @@ class App extends React.Component {
     }
     this.getCaregivers = this.getCaregivers.bind(this)
     this.handleAddCaregiver = this.handleAddCaregiver.bind(this)
+    this.handleEditCaregiver = this.handleEditCaregiver.bind(this)
+    this.deleteCaregiver = this.deleteCaregiver.bind(this)
   }
 
-    componentDidMount(){
+    componentDidMount(){ //populates caregivers list on load.
         this.getCaregivers()
       }
 
+// API Connection Functions:
+
     async getCaregivers (){
         try {
-          let response = await fetch(`${baseURL}/caregivers`)
+          let response = await fetch(`${baseURL}/perennial-api`)
           let data = await response.json()
           this.setState({caregivers: data})
         }catch(e){
@@ -41,7 +46,7 @@ class App extends React.Component {
 
     async handleAddCaregiver(caregiver) {
         try {
-            let response = await fetch(`${baseURL}/caregivers`, {
+            let response = await fetch(`${baseURL}/perennial-api`, {
                 method: 'POST',
                 body: JSON.stringify(caregiver),
                 headers: {
@@ -58,15 +63,53 @@ class App extends React.Component {
         }
     }
 
+    async handleEditCaregiver(caregiver) {
+        try {
+            let response = await fetch(`${baseURL}/perennial-api/${caregiver._id}`, {
+                method: 'PUT',
+                body: JSON.stringify(caregiver),
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            })
+            let updatedCaregiver = response.json()
+            let updatedCaregiverIndex = this.state.caregivers.findIndex(caregiver => caregiver._id === updatedCaregiver._id)
+            let tempDatabase = this.state.caregivers
+            tempDatabase[updatedCaregiverIndex] = updatedCaregiver
+            this.setState({
+                caregivers: tempDatabase
+            })
+        } catch(e) {
+            console.error(e);
+        }
+    }
+
+    async deleteCaregiver(caregiver) {
+        try {
+            let response = await fetch(`${baseURL}/perennial-api/${caregiver._id}`, {
+                method: 'DELETE'
+            })
+            let deletedCaregiver = response.json()
+            let deletedCaregiverIndex = this.state.caregivers.findIndex(caregiver => caregiver._id === deletedCaregiver._id)
+            let tempDatabase = this.state.caregivers
+            tempDatabase.splice(deletedCaregiverIndex, 1)
+            this.setState({
+                caregivers: tempDatabase
+            })
+        } catch (e) {
+            console.error(e);
+        }
+    }
+
 render () {
   return (
     <React.Fragment>
     <h1>Main Page</h1>
-    <NewCaregiver />
+    <NewCaregiver handleAddCaregiver={this.handleAddCaregiver} />
 
-    <ShowCaregivers />
+    <ShowCaregivers caregivers={this.state.caregivers}/>
 
-    <UpdateCaregiver />
+    <UpdateCaregiver handleEditCaregiver={this.handleEditCaregiver} deleteCaregiver={this.deleteCaregiver}/>
     <h3>Brought to you by Main Pages, llc., a subsidiary of the NRJ (Nathaniel/Ric/Johnny) Corporation.</h3>
     </React.Fragment>
   )
