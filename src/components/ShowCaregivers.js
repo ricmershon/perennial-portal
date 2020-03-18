63d7334b806eaaef2a6ab69861e9587c03ed51dc
Johnny Nichol <johnnichol@johns-mbp.home>
2020-03-18 11:01:19 -0400

    working on cards

--- src/components/ShowCaregivers.js
+++ src/components/ShowCaregivers.js
@@ -10,7 +10,33 @@ class ShowCaregivers extends React.Component {
 
   render () {
     return (
-      <h1>Caregiver cards</h1>
+        <div className="container">
+            <h1>Caregiver cards</h1>
+            <div>
+                {this.props.caregivers.map((caregiver, index) => {
+                    return(
+                        <Card style={{ width: '18rem' }}>
+                        <Card.Body>
+                        <Card.Title>{caregiver.firstName}{caregiver.lastName}</Card.Title>
+                        <Card.Text>
+                        {caregiver.description}
+                        </Card.Text>
+                        </Card.Body>
+                        <ListGroup className="list-group-flush">
+                        <ListGroupItem>{caregiver.phone}</ListGroupItem>
+                        <ListGroupItem>{caregiver.email}</ListGroupItem>
+                        <ListGroupItem>{caregiver.location}</ListGroupItem>
+                        <ListGroupItem>{caregiver.rating}</ListGroupItem>
+                        </ListGroup>
+                        <Card.Body>
+                        <Card.Link href="#">Edit</Card.Link>
+                        <Card.Link href="#">Delete</Card.Link>
+                        </Card.Body>
+                        </Card>
+                    )
+                })}
+            </div>
+        </div>
     )
   }
 }
