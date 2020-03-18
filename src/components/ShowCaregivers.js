86467f6568184623b8ed7eefd61175c36a59e2d4
Johnny Nichol <johnnichol@johns-mbp.home>
2020-03-18 12:03:07 -0400

    1st draft of caregiver cards

--- src/components/ShowCaregivers.js
+++ src/components/ShowCaregivers.js
@@ -1,42 +1,49 @@
 import React from 'react'
+import Card from 'react-bootstrap/Card'
+import ListGroup from 'react-bootstrap/ListGroup'
+import ListGroupItem from 'react-bootstrap/ListGroupItem'
+import CardColumns from 'react-bootstrap/CardColumns'
+
 
 class ShowCaregivers extends React.Component {
   constructor (props) {
-  super(props)
-  this.state = {
-    name: ''
+    super(props)
+    this.state = {
+      name: ''
+    }
   }
-}
 
   render () {
     return (
-        <div className="container">
-            <h1>Caregiver cards</h1>
-            <div>
-                {this.props.caregivers.map((caregiver, index) => {
-                    return(
-                        <Card style={{ width: '18rem' }}>
-                        <Card.Body>
-                        <Card.Title>{caregiver.firstName}{caregiver.lastName}</Card.Title>
-                        <Card.Text>
-                        {caregiver.description}
-                        </Card.Text>
-                        </Card.Body>
-                        <ListGroup className="list-group-flush">
-                        <ListGroupItem>{caregiver.phone}</ListGroupItem>
-                        <ListGroupItem>{caregiver.email}</ListGroupItem>
-                        <ListGroupItem>{caregiver.location}</ListGroupItem>
-                        <ListGroupItem>{caregiver.rating}</ListGroupItem>
-                        </ListGroup>
-                        <Card.Body>
-                        <Card.Link href="#">Edit</Card.Link>
-                        <Card.Link href="#">Delete</Card.Link>
-                        </Card.Body>
-                        </Card>
-                    )
-                })}
-            </div>
-        </div>
+      <div className="container">
+      <CardColumns>
+
+      {this.props.caregivers.map((caregiver, index) => {
+        return(
+          <Card bg="info" border="success" style={{width: '14rem'}} className="overflow-auto">
+          <Card.Body>
+          <Card.Title>Name: {caregiver.contact.firstName} {caregiver.contact.lastName}</Card.Title>
+          <Card.Text>
+          {caregiver.description}
+          </Card.Text>
+          </Card.Body>
+          <ListGroup className="list-group-flush">
+          <ListGroupItem>Phone: {caregiver.phone}</ListGroupItem>
+          <ListGroupItem>Email: {caregiver.email}</ListGroupItem>
+          <ListGroupItem>Location: {caregiver.location}</ListGroupItem>
+          <ListGroupItem>Rating: {caregiver.rating} stars</ListGroupItem>
+          </ListGroup>
+          <Card.Body>
+          <Card.Link href="#">Edit</Card.Link>
+          <Card.Link href="#">Delete</Card.Link>
+          </Card.Body>
+          </Card>
+
+        )
+      })}
+      </CardColumns>
+
+      </div>
     )
   }
 }
