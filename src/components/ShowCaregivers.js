20eeb6e8dbf63efdfba54d3eb3e096103c436fe9
Johnny Nichol <johnnichol@johns-mbp.home>
2020-03-18 21:53:17 -0400

    finalized(?) shanges to Show Cards

--- src/components/ShowCaregivers.js
+++ src/components/ShowCaregivers.js
@@ -1,8 +1,6 @@
 import React from 'react'
-import Card from 'react-bootstrap/Card'
-import ListGroup from 'react-bootstrap/ListGroup'
-import ListGroupItem from 'react-bootstrap/ListGroupItem'
 import CardColumns from 'react-bootstrap/CardColumns'
+import Caregiver from './Caregiver.js'
 
 
 class ShowCaregivers extends React.Component {
